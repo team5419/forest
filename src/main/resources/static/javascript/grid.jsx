3fd67c80d6a7f1efd34f097e23ca81e03d167f7d
Mattiwos Belachew <mattiwosbelachew@lich.gateway.sonic.net>
2020-04-16 16:24:57 -0700

    setting up env for gridstack

---
+++ src/main/resources/static/javascript/grid.jsx
@@ -0,0 +1,16 @@
+import PageUtils from "page-utils";
+
+export default class Grid extends React.Component{ //will go to page-utls
+    
+    constructor(widgets){
+        
+
+    }
+
+    render(){
+        return {
+
+
+        }
+    }
+}
