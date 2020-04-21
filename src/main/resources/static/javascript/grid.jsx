447d18baf3a649e4415e5ceab257a0eefe7e23bf
Mattiwos Belachew <mattiwosbelachew@lich.gateway.sonic.net>
2020-04-21 16:49:45 -0700

    taking on one bug at a time worked on getting grid working has errosrs cyrr

--- src/main/resources/static/javascript/grid.jsx
+++ src/main/resources/static/javascript/grid.jsx
@@ -1,18 +1,28 @@
 import PageUtils from "page-utils";
+import React, { Component } from 'react';
 
-export default class Grid extends React.Component{ //will go to page-utls
+export default class Grid extends Component{ //will go to page-utls
     
     constructor(props){
         super(props)
-
+        
     }
 
     render(){
         return (
-            <div className="grid-stack">
-                {PageUtils.renderWidgets()}
+            <div>
+                <div className="grid-stack"></div>
+                {this.init()}
+
             </div>
+            
         )
             
+    }
+    init(){
+        this.grid = GridStack.init();
+        this.grid.addWidget(el, 0, 0, 3, 2, true);
+        grid.addWidget('<div class="grid-stack-item"><div class="grid-stack-item-content"> test </div></div>', {width: 2})
+
     }
 }
